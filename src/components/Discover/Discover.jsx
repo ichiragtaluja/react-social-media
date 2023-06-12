e3351f011c456ce0bba7a2f89e60d5028a875362
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-06-12 12:08:54 +0530

    style: add discover section layout

--- src/components/Discover/Discover.jsx
+++ src/components/Discover/Discover.jsx
@@ -1,6 +1,37 @@
 import "./Discover.css";
 import React from "react";
+import { useUser } from "../../contexts/UserProvider";
 
 export const Discover = () => {
-  return <div className="discover">Discover</div>;
+  const { userState } = useUser();
+  const whoToFollow = userState?.allUsers;
+  console.log(whoToFollow);
+
+  return (
+    <div className="discover">
+      <div className="discover-container">
+        <div className="discover-header">
+          <p>Who to follow?</p>
+        </div>
+        <div className="discover-body">
+          {whoToFollow?.map((user) => (
+            <div key={user?._id} className="discover-user-card">
+              <div className="discover-user-img-container">
+                <img src={user?.avatarURL} />
+              </div>
+              <div className="user-name-username-container">
+                <p className="name">
+                  {user?.firstName} {user?.lastName}
+                </p>
+                <p className="username">@{user?.username}</p>
+              </div>
+              <div>
+                <button>Follow</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
