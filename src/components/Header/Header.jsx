fffb6884640239a2b244499d22e3f920eca2f6a0
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-06-18 17:05:19 +0530

    style: add css to header and navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
 import "./Header.css";
 import React from "react";
+import { SiDuckdb } from "react-icons/si";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
-  return <div className="header">Header</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="header">
+      <SiDuckdb
+        onClick={() => navigate("/")}
+        className="header-logo"
+        size={25}
+      />
+      <p onClick={() => navigate("/")}>Quack</p>
+    </div>
+  );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,67 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import React from "react";
 import { useAuth } from "../../contexts/AuthProvider";
-import { IoIosAddCircle } from "react-icons/io";
+import { FaFeather } from "react-icons/fa";
 import { CreatePostForm } from "../CreatePostForm/CreatePostForm";
+import { RiHomeWifiLine } from "react-icons/ri";
+import { BiSearch } from "react-icons/bi";
+import { HiOutlineBookmark } from "react-icons/hi";
+import { CgProfile } from "react-icons/cg";
+import { IoMdLogOut } from "react-icons/io";
 
 export const Navbar = () => {
   const [isCreateNewPostClicked, setIsCreateNewPostClicked] = useState(false);
 
   const { auth } = useAuth();
+  const getActiveStyle = ({ isActive }) => ({
+    color: isActive ? "rgb(29, 155, 240)" : "white",
+  });
   return (
     <nav className="navbar">
       <ul>
         <li>
-          <Link to="/">Logo</Link>
+          <NavLink className="navlink" style={getActiveStyle} to="/">
+            {<RiHomeWifiLine className="navlink-icon" />}
+            <p>Home</p>
+          </NavLink>
         </li>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink className="navlink" style={getActiveStyle} to="/explore">
+            <BiSearch className="navlink-icon" />
+            <p>Explore</p>
+          </NavLink>
         </li>
         <li>
-          <Link to="/explore">Explore</Link>
+          <NavLink className="navlink" style={getActiveStyle} to="/bookmark">
+            <HiOutlineBookmark className="navlink-icon" />
+            <p>Bookmark</p>
+          </NavLink>
         </li>
         <li>
-          <Link to="/bookmark">Bookmark</Link>
+          <NavLink
+            className="navlink"
+            style={getActiveStyle}
+            to={`/profile/${auth.username}`}
+          >
+            <CgProfile className="navlink-icon" />
+            <p>Profile</p>
+          </NavLink>
         </li>
         <li>
-          <Link to={`/profile/${auth.username}`}>Profile</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
+          <NavLink className="navlink" style={getActiveStyle} to="/logout">
+            <IoMdLogOut className="navlink-icon" />
+            <p>Logout</p>
+          </NavLink>
         </li>
       </ul>
       <button
+        className="create-new-post-btn"
         onClick={() => setIsCreateNewPostClicked(!isCreateNewPostClicked)}
       >
-        <IoIosAddCircle />
-        <span>Create New Post</span>
+        <FaFeather className="feather-icon" />
+        <span>New Post</span>
       </button>
       {isCreateNewPostClicked && (
         <div className="create-post-modal">
